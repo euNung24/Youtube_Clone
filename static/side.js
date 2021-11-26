f5a7063b92be219c3da38e11d53337670c1d08d2
EunYoung <pandamon24@naver.com>
2021-11-26 14:16:28 +0900

    css 및 side 경로 수정

--- static/side.js
+++ static/side.js
@@ -6,7 +6,6 @@ const contents = document.querySelector(".contents");
 function openSlide() {
   simpleSlide.classList.toggle("open");
   wideSide.classList.toggle("open");
-  contents.classList.toggle("open");
 }
 
 menu.addEventListener("click", openSlide);
