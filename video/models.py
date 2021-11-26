f7b2e31c0139b8b2f5f23356dc3f8bc21ce22cf5
EunYoung <pandamon24@naver.com>
2021-11-26 16:24:21 +0900

    video_key 가져오는 방법 수정

--- video/models.py
+++ video/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 class Video(models.Model):
   title = models.CharField(max_length=200)
-  video_key = models.CharField(max_length=12)
+  video_key = models.TextField()
+
+  def change_url(self):
+    link = self.video_key
+    split_link = link.split("/")[-1]
+    return f'//www.youtube.com/embed/{split_link}'
+
+  def get_only_key(self):
+    return self.video_key.split("/")[-1]
